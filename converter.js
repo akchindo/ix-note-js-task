e15768fcbf223ed75cfdbe472f54b55a97f18f10
akchindo <abdullahichindo16@gmail.com>
2021-05-18 16:18:57 +0100

    initial commit

---
+++ converter.js
@@ -0,0 +1,16 @@
+function convertFahrToCelsius(parameter) {
+
+  const toCelsFormula = (parameter - 32) / 1.8
+  const varType = typeof parameter
+  
+  
+  if(isFinite(toCelsFormula)) {
+    console.log(toCelsFormula.toFixed(4));
+        
+} else {
+  console.log(`${toCelsFormula} is not a valid number but a ${varType}`);
+}
+
+}
+
+convertFahrToCelsius({1:1})
